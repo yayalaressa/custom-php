b8699eabc0eaac9f5ea3a417ada48f311e3b57f8
Yaya Laressa <yayalaressa2015@gmail.com>
2021-06-02 01:05:45 +0700

    Upload update

--- index.php
+++ index.php
@@ -1,21 +1,17 @@
 <?php
 
+// Define 
 define('BASEPATH', dirname(__FILE__));
-
+$config_file = 'config/config.ini';
 // Require composer autoloader
 require_once BASEPATH . '/system/vendor/autoload.php';
-
 // vQmod
-require_once(BASEPATH . '/vqmod/vqmod.php');
 VQMod::bootup();
-
 // Create Router instance
 $router = new \Bramus\Router\Router();
-
+// Class Theme
+$theme = new Theme();
 // VQMODDED Startup
 require_once(VQMod::modCheck(BASEPATH . '/system/app.php'));
-
-// Run it!
-$router->run();
 
 ?>
