048862ab6181eca462c7f8490b80ea94ca94fb4e
Yaya Laressa <yayalaressa2015@gmail.com>
2021-06-04 16:26:29 +0700

    upload beta

--- system/core/admin.php
+++ system/core/admin.php
@@ -1,48 +1,54 @@
 <?php if (!defined('BASEPATH')) die('Access Denied!');
 
-class Admin {
+class Admin
+{
 
-	private static $instance;
+    private static $instance;
     public $theme_data = array();
 
-	function __construct() {
-		self::$instance = $this;
-	}
-	
-	private function set($name, $value){
-		$this->theme_data[$name] = $value;
-	}
+    function __construct()
+    {
+        self::$instance = $this;
+    }
 
-	private function load($theme = '', $view = '' , $view_data = array(), $return = FALSE)
-	{  
-		$this->set('content', view($view, $view_data, TRUE));	
-		return view($theme, $this->theme_data, $return);
-	}
-		
-	public function render($view = '', $data = array(), $return = FALSE)
-	{
-		if(config('minify.enable') == 'true') {
-			$html = $this->load('system/admin/views/overview', 'system/admin/views/'.$view, $data, TRUE);
-			echo Minifier::html($html);
-		} else { 
-			$this->load('system/admin/views/overview', 'system/admin/views/'.$view, $data, $return);
-		}
-	}
-	
-	public function model($model)
+    private function set($name, $value)
     {
-       load_model($model, Admin::get_instance());
+        $this->theme_data[$name] = $value;
     }
-    
-	public function library($library)
+
+    private function load($theme = '', $view = '', $view_data = array() , $return = false)
     {
-       load_library($library, Admin::get_instance());
+        $this->set('content', view($view, $view_data, true));
+        return view($theme, $this->theme_data, $return);
     }
-    
-	public static function get_instance()
-	{
-		return self::$instance;
-	}
+
+    public function render($view = '', $data = array() , $return = false)
+    {
+        if (config('minify.enable') == 'true')
+        {
+            $html = $this->load('system/admin/views/overview', 'system/admin/views/' . $view, $data, true);
+            echo Minifier::html($html);
+        }
+        else
+        {
+            $this->load('system/admin/views/overview', 'system/admin/views/' . $view, $data, $return);
+        }
+    }
+
+    public function model($model)
+    {
+        load_model($model, Admin::get_instance());
+    }
+
+    public function library($library)
+    {
+        load_library($library, Admin::get_instance());
+    }
+
+    public static function get_instance()
+    {
+        return self::$instance;
+    }
 
 }
 
--- system/core/app.php
+++ system/core/app.php
@@ -1,49 +1,56 @@
 <?php
 if (!defined('BASEPATH')) die('Access Denied!');
 
-class App {
+class App
+{
 
-	private static $instance;
+    private static $instance;
     public $theme_data = array();
 
-	function __construct() {
-		self::$instance = $this;
-
-	}
-	
-	private function set($name, $value){
-		$this->theme_data[$name] = $value;
-	}
-
-	private function load($theme = '', $view = '' , $view_data = array(), $return = FALSE)
-	{  
-		$this->set('content', view($view, $view_data, TRUE));	
-		return view($theme, $this->theme_data, $return);
-	}
-	
-	public function render($view = '', $data = array(), $return = FALSE)
-	{
-		if(config('minify.enable') == 'true') {
-			$html = $this->load('themes/default/overview', 'themes/default/'.$view, $data, TRUE);
-			echo Minifier::html($html);
-		} else { 
-			$this->load('themes/default/overview', 'themes/default/'.$view, $data, $return);
-		}
-	}
-	
-	public function model($model)
+    function __construct()
     {
-       load_model($model, App::get_instance());
+        self::$instance = $this;
+
     }
-    
-	public function library($library)
+
+    private function set($name, $value)
     {
-       load_library($library, App::get_instance());
+        $this->theme_data[$name] = $value;
     }
-    
-	public static function get_instance()
-	{
-		return self::$instance;
-	}
+
+    private function load($theme = '', $view = '', $view_data = array() , $return = false)
+    {
+        $this->set('content', view($view, $view_data, true));
+        return view($theme, $this->theme_data, $return);
+    }
+
+    public function render($view = '', $data = array() , $return = false)
+    {
+        if (config('minify.enable') == 'true')
+        {
+            $html = $this->load('themes/default/overview', 'themes/default/' . $view, $data, true);
+            echo Minifier::html($html);
+        }
+        else
+        {
+            $this->load('themes/default/overview', 'themes/default/' . $view, $data, $return);
+        }
+    }
+
+    public function model($model)
+    {
+        load_model($model, App::get_instance());
+    }
+
+    public function library($library)
+    {
+        load_library($library, App::get_instance());
+    }
+
+    public static function get_instance()
+    {
+        return self::$instance;
+    }
 
 }
+
